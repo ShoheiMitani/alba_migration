8be20fa53095f6ead526ff0f9d7082426fc11dfa
ShoheiMitani <mitani49@gmail.com>
2025-05-13 10:02:18 +0000

    テスト: 不要なテストケースを削除してrspec用のコマンドを追加

--- spec/alba_migration_spec.rb
+++ spec/alba_migration_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe AlbaMigration do
   it "has a version number" do
     expect(AlbaMigration::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
