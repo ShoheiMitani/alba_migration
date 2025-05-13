2a6de6f81a991087d632c0fb92a3484ebb4eaac7
ShoheiMitani <mitani49@gmail.com>
2025-05-13 18:21:50 +0900

    feat: first commit
    bundle gem with option --mit --test=rspec --ci=github --linter=standard --git

---
+++ spec/alba_migration_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe AlbaMigration do
+  it "has a version number" do
+    expect(AlbaMigration::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
